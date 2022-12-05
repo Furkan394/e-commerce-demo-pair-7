485570df46f4c56944c49cf7b43a0b53ad236f5f
emre.cakiroglu <furkanncakiroglu@gmail.com>
2022-12-05 14:28:52 +0300

    added/post delivery option with request-response pattern

---
+++ src/main/java/com/etiya/ecommercedemopair7/api/controllers/DeliveryOptionsController.java
@@ -0,0 +1,29 @@
+package com.etiya.ecommercedemopair7.api.controllers;
+
+import com.etiya.ecommercedemopair7.business.abstracts.IDeliveryOptionService;
+import com.etiya.ecommercedemopair7.business.request.deliveryOptions.AddDeliveryOptionRequest;
+import com.etiya.ecommercedemopair7.business.response.deliveryOptions.AddDeliveryOptionResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/delivery-options")
+public class DeliveryOptionsController {
+
+    private IDeliveryOptionService deliveryOptionService;
+
+    @Autowired
+    public DeliveryOptionsController(IDeliveryOptionService deliveryOptionService) {
+        this.deliveryOptionService = deliveryOptionService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<AddDeliveryOptionResponse> add(@RequestBody AddDeliveryOptionRequest addDeliveryOptionRequest) {
+        return new ResponseEntity<AddDeliveryOptionResponse>(deliveryOptionService.add(addDeliveryOptionRequest), HttpStatus.CREATED);
+    }
+}
