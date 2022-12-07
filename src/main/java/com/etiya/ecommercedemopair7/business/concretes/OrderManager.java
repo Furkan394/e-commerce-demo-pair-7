8ba050938da5d5c0d379d26452f6798308dca4ec
emre.cakiroglu <furkanncakiroglu@gmail.com>
2022-12-07 15:05:20 +0300

    Changes by zeynep.han

--- src/main/java/com/etiya/ecommercedemopair7/business/concretes/OrderManager.java
+++ src/main/java/com/etiya/ecommercedemopair7/business/concretes/OrderManager.java
@@ -4,6 +4,8 @@ import com.etiya.ecommercedemopair7.business.abstracts.IDeliveryOptionService;
 import com.etiya.ecommercedemopair7.business.abstracts.IOrderService;
 import com.etiya.ecommercedemopair7.business.request.orders.AddOrderRequest;
 import com.etiya.ecommercedemopair7.business.response.orders.AddOrderResponse;
+import com.etiya.ecommercedemopair7.core.utilities.mapping.IModelMapperService;
+import com.etiya.ecommercedemopair7.entities.concretes.Address;
 import com.etiya.ecommercedemopair7.entities.concretes.DeliveryOption;
 import com.etiya.ecommercedemopair7.entities.concretes.Order;
 import com.etiya.ecommercedemopair7.repository.abstracts.IOrderRepository;
@@ -12,14 +14,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderManager implements IOrderService {
-
     private IOrderRepository orderRepository;
     private IDeliveryOptionService deliveryOptionService;
 
     @Autowired
-    public OrderManager(IOrderRepository orderRepository, IDeliveryOptionService deliveryOptionService) {
+    public OrderManager(IOrderRepository orderRepository, IDeliveryOptionService deliveryOptionService, IAddressService addressService, IModelMapperService mapper) {
         this.orderRepository = orderRepository;
         this.deliveryOptionService = deliveryOptionService;
+        this.addressService = addressService;
+        this.mapper = mapper;
     }
 
     @Override
@@ -34,8 +37,7 @@ public class OrderManager implements IOrderService {
 
         Order savedOrder = orderRepository.save(order);
 
-        AddOrderResponse response = new AddOrderResponse(savedOrder.getId(), savedOrder.getOrderNumber(), savedOrder.getTotalPrice()
-                , savedOrder.getOrderDate(), savedOrder.getDeliveryOption().getId(), savedOrder.getOrderAddress().getId(), savedOrder.getInvoiceAddress().getId());
+        AddOrderResponse response = mapper.forResponse().map(savedOrder, AddOrderResponse.class);
 
         return response;
     }
