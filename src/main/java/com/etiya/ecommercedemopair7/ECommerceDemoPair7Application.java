40750d034dab89ba83f6fb5cee4a004157834579
emre.cakiroglu <furkanncakiroglu@gmail.com>
2022-12-12 10:53:08 +0300

    added global exception handling

--- src/main/java/com/etiya/ecommercedemopair7/ECommerceDemoPair7Application.java
+++ src/main/java/com/etiya/ecommercedemopair7/ECommerceDemoPair7Application.java
@@ -1,19 +1,50 @@
 package com.etiya.ecommercedemopair7;
 
+import com.etiya.ecommercedemopair7.core.utilities.exceptions.BusinessException;
+import com.etiya.ecommercedemopair7.core.utilities.results.ErrorDataResult;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestControllerAdvice
 public class ECommerceDemoPair7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceDemoPair7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceDemoPair7Application.class, args);
+    }
 
-	@Bean
-	public ModelMapper getModelMapper(){
-		return new ModelMapper();
-	}
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ErrorDataResult<>(validationErrors, "VALIDATION_EXCEPTION");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleBusinessException(BusinessException exception) {
+        ErrorDataResult<Object> businessErrors = new ErrorDataResult<>(exception.getMessage(), "BUSINESS_EXCEPTION");
+        return businessErrors;
+    }
 }
--- src/main/java/com/etiya/ecommercedemopair7/business/concretes/BasketManager.java
+++ src/main/java/com/etiya/ecommercedemopair7/business/concretes/BasketManager.java
@@ -6,6 +6,7 @@ import com.etiya.ecommercedemopair7.business.constants.Messages;
 import com.etiya.ecommercedemopair7.business.request.baskets.AddBasketRequest;
 import com.etiya.ecommercedemopair7.business.response.baskets.AddBasketResponse;
 import com.etiya.ecommercedemopair7.business.response.baskets.GetAllBasketResponse;
+import com.etiya.ecommercedemopair7.core.utilities.exceptions.BusinessException;
 import com.etiya.ecommercedemopair7.core.utilities.mapping.IModelMapperService;
 import com.etiya.ecommercedemopair7.core.utilities.results.DataResult;
 import com.etiya.ecommercedemopair7.core.utilities.results.SuccessDataResult;
@@ -69,7 +70,7 @@ public class BasketManager implements IBasketService {
         try {
             currentBasket = this.basketRepository.findById(basketId).get();
         } catch (Exception e) {
-            throw new RuntimeException(Messages.Basket.basketNotFound);
+            throw new BusinessException(Messages.Basket.basketNotFound);
         }
         return currentBasket;
     }
